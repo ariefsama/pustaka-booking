37fc757ba489d11344d031e984cc2fbebb1df928
ariefsama <muhariffi@gmail.com>
2023-04-02 22:33:04 +0000

    menambahkan view latihan1

---
+++ application/views/latihan1.php
@@ -0,0 +1,27 @@
+<?php 
+defined('BASEPATH') OR  exit('No direct script access allowed');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hasil latihan 1</title>
+</head>
+<body>
+  <div id="container">
+    <h1>Hasil latihan 1</h1>
+
+      <h4>Hasil code sederhana MVC test penjumlahan</h4>
+    <div id="body">
+      <p>Nilai 1 = <?= $nilai1 ?></p>
+      <p>Nilai 2 = <?= $nilai2 ?></p>
+      <p>Hasil = <?= $hasil ?></p>
+      
+    </div>
+  </div>
+  
+</body>
+</html>
